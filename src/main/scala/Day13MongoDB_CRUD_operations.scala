dd715cbc684c158a47e955f198c04db021206b8f
Eleonora <eleonora.sturo@gmail.com>
2022-07-11 19:31:54 +0300

    MongoDB read all Restaurants

--- src/main/scala/Day13MongoDB_CRUD_operations.scala
+++ src/main/scala/Day13MongoDB_CRUD_operations.scala
@@ -34,21 +34,31 @@ object Day13MongoDB_CRUD_operations extends App{
           },
       (e: Throwable) => println(s"Query error: $e"),
       //this is what we can do after the query is finished
-      () => {
-        println("Closing after last query")
-        //so idea is to close after last query is complete
-        val allRestaurants = resultsBuffer.toArray
-        println(s"We got ${allRestaurants.length} restaurants total")
-        client.close()
-      }
+      afterQuerySuccess // NOTICE in functional style I do not call the function here
+      //I just tell my subscription WHAT to call (it means I don't need to write parenthesis ())
     )
   //this line should run before our closing line
-  println("Query is still running")
+  println("Query is still running - Data is not guaranteed to be ready")
+  // println(s"Buffer length is ${resultsBuffer.length}")
+//  sleep(2000)
+//  println(s"Buffer length is ${resultsBuffer.length}")
+  //looks like data ie returned in one big swell swoop so buffer is 0 then very quickly it fills up
 
-  //TODO move on success query here
   def afterQuerySuccess() : Unit = {
-
+    println("Closing after last query")
+    //so idea is to close after last query is complete
+    val allRestaurantsDocs = resultsBuffer.toArray
+    println(s"We got ${allRestaurantsDocs.length} restaurants total")
+    println("First restaurant")
+    println(allRestaurantsDocs.head.toJson())
+    val savePath = "src/resources/json/restaurants.json"
+    val restaurantJSON = allRestaurantsDocs.map(_.toJson()) //so we convert / map all documents to JSON strings
+    Util.saveLines(savePath, restaurantJSON)//so we extract all of the collection and save it for later use
+    client.close()
   }
+
+
+
 
 
 }
